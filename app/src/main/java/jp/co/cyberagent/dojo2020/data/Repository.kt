1e54dab2a3df47eac467aed6fbb01331d907e1ca
ShebangDog <e1n18088@oit.ac.jp>
2020-09-01 14:52:42 +0900

    Repositoryの実装(roomのみを想定）

--- app/src/main/java/jp/co/cyberagent/dojo2020/data/Repository.kt
+++ app/src/main/java/jp/co/cyberagent/dojo2020/data/Repository.kt
@@ -3,12 +3,16 @@ package jp.co.cyberagent.dojo2020.data
 import jp.co.cyberagent.dojo2020.data.model.Memo
 import kotlinx.coroutines.flow.Flow
 
-class Repository(private val localDataSource: DataSource, private val remoteDataSource: DataSource) : DataSource {
+class Repository(
+    private val localDataSource: DataSource,
+    private val remoteDataSource: DataSource
+) : DataSource {
+
     override suspend fun save(memo: Memo) {
-        TODO("Not yet implemented")
+        localDataSource.save(memo)
     }
 
     override suspend fun fetchAll(): Flow<List<Memo>> {
-        TODO("Not yet implemented")
+        return localDataSource.fetchAll()
     }
 }

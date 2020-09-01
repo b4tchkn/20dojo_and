339abac1a672292dd6e8934eb974cb885bb8e282
ShebangDog <e1n18088@oit.ac.jp>
2020-09-02 01:53:25 +0900

    変数名を変更する(ViewModel から HomeViewModel)

--- app/src/main/java/jp/co/cyberagent/dojo2020/ui/MainActivity.kt
+++ app/src/main/java/jp/co/cyberagent/dojo2020/ui/MainActivity.kt
@@ -3,7 +3,6 @@ package jp.co.cyberagent.dojo2020.ui
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.observe
 import jp.co.cyberagent.dojo2020.data.model.Memo
 import jp.co.cyberagent.dojo2020.databinding.ActivityMainBinding
 import jp.co.cyberagent.dojo2020.ui.home.HomeViewModel
@@ -12,7 +11,7 @@ import jp.co.cyberagent.dojo2020.ui.home.HomeViewModelFactory
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
-    private val viewModel: HomeViewModel by viewModels {
+    private val homeViewModel: HomeViewModel by viewModels {
         HomeViewModelFactory(this, Bundle(), this)
     }
 
@@ -23,7 +22,7 @@ class MainActivity : AppCompatActivity() {
 
         with(binding) {
 
-            viewModel.liveData.observe(this@MainActivity) {
+            homeViewModel.liveData.observe(this@MainActivity) {
                 val firstMemo = it.randomOrNull() ?: return@observe
 
                 firstMemoTitle.text = firstMemo.title
@@ -38,14 +37,14 @@ class MainActivity : AppCompatActivity() {
             }
 
             reloadButton.setOnClickListener {
-                viewModel.fetchMemoList()
+                homeViewModel.fetchMemoList()
             }
 
             saveButton.setOnClickListener {
                 val title = titleEditText.text.toString()
                 val memo = createWithTitle(title)
 
-                viewModel.saveMemo(memo)
+                homeViewModel.saveMemo(memo)
             }
         }
     }
@@ -53,4 +52,5 @@ class MainActivity : AppCompatActivity() {
     private fun createWithTitle(title: String): Memo {
         return Memo(title, "contents", 0.256)
     }
+
 }

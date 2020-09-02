57dde807a5f603313627231a8f30ca58de0884ee
aaa165 <b1802067@planet.kanazawa-it.ac.jp>
2020-09-02 13:57:34 +0900

    Recyclerviewのアダプタ改良版

--- app/src/main/java/jp/co/cyberagent/dojo2020/ui/main/RecyclerAdapter.kt
+++ app/src/main/java/jp/co/cyberagent/dojo2020/ui/main/RecyclerAdapter.kt
@@ -1,46 +1,25 @@
 package jp.co.cyberagent.dojo2020.ui.main
 
-import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import jp.co.cyberagent.dojo2020.R
 
-class RecyclerAdapter(private val context: Context,private val itemClickListner: RecyclerViewHolder.ItemClickListner,private val itemList:List<String>):RecyclerView.Adapter<RecyclerViewHolder>() {
-    private var mRecyclerView: RecyclerView? = null
-
-    override fun onAttachedToRecyclerView(recyclerView: RecyclerView) {
-        super.onAttachedToRecyclerView(recyclerView)
-        mRecyclerView = recyclerView
-    }
-
-    override fun onDetachedFromRecyclerView(recyclerView: RecyclerView) {
-        super.onDetachedFromRecyclerView(recyclerView)
-        mRecyclerView = null
+class RecyclerAdapter(
+    private val myDataset: Recyclerviewmain,
+    recyclerviewmain: Recyclerviewmain,
+    a: MutableList<String>
+):RecyclerView.Adapter<RecyclerViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerViewHolder {
+        val textView = LayoutInflater.from(parent.context).inflate(R.layout.list_item,parent,false) as TextView
+        return RecyclerViewHolder(textView)
     }
 
     override fun onBindViewHolder(holder: RecyclerViewHolder, position: Int) {
-        holder.let {
-            it.itemTextView.text = itemList.get(position)
-        }
+        holder.itemTextView.text = myDataset[position]
     }
 
-    override fun getItemCount(): Int {
-        return itemList.size
-    }
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerViewHolder {
-
-        val layoutInflater = LayoutInflater.from(context)
-        val mView = layoutInflater.inflate(R.layout.list_item,parent,false)
-
-        mView.setOnClickListener { view ->
-            mRecyclerView?.let {
-                itemClickListner.onItemClick(view, it.getChildAdapterPosition(view))
-            }
-        }
-
-        return RecyclerViewHolder(mView)
-    }
+    override fun getItemCount() = myDataset.size
 
 }

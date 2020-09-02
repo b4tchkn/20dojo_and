34d4ba4f15136756fecadb9b9a3d65d4362998c7
aaa165 <b1802067@planet.kanazawa-it.ac.jp>
2020-09-02 22:24:20 +0900

    Recyclerviewのアダプタ改３

--- app/src/main/java/jp/co/cyberagent/dojo2020/ui/main/RecyclerAdapter.kt
+++ app/src/main/java/jp/co/cyberagent/dojo2020/ui/main/RecyclerAdapter.kt
@@ -6,16 +6,16 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import jp.co.cyberagent.dojo2020.R
 
-class RecyclerAdapter(private val a: MutableList<String>):RecyclerView.Adapter<RecyclerViewHolder>() {
+class RecyclerAdapter(private val list: List<String>):RecyclerView.Adapter<RecyclerViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerViewHolder {
-        val textView = LayoutInflater.from(parent.context).inflate(R.layout.list_item,parent,false)
-        return RecyclerViewHolder(textView)
+        val View = LayoutInflater.from(parent.context).inflate(R.layout.list_item,parent,false)
+        return RecyclerViewHolder(View)
     }
 
     override fun onBindViewHolder(holder: RecyclerViewHolder, position: Int) {
-        holder.itemTextView.text = a[position]
+        holder.itemTextView.text = list[position]
     }
 
-    override fun getItemCount() = a.size
+    override fun getItemCount() = list.size
 
 }

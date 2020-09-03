e0402855d4304bbae2bacbbee3203a09317a41e8
ShebangDog <e1n18088@oit.ac.jp>
2020-09-04 04:07:46 +0900

    FirebaseUserInfoDataSourceへ名前を変更する

--- app/src/main/java/jp/co/cyberagent/dojo2020/data/remote/auth/FirebaseUserInfoDataSource.kt
+++ app/src/main/java/jp/co/cyberagent/dojo2020/data/remote/auth/FirebaseUserInfoDataSource.kt
@@ -3,6 +3,6 @@ package jp.co.cyberagent.dojo2020.data.remote.auth
 import jp.co.cyberagent.dojo2020.data.model.FirebaseUserInfo
 import kotlinx.coroutines.flow.Flow
 
-interface AuthDataSource {
+interface FirebaseUserInfoDataSource {
     fun fetchFirebaseUserInfo(): Flow<FirebaseUserInfo>
 }
--- app/src/main/java/jp/co/cyberagent/dojo2020/ui/home/HomeViewModel.kt
+++ app/src/main/java/jp/co/cyberagent/dojo2020/ui/home/HomeViewModel.kt
@@ -5,16 +5,16 @@ import androidx.lifecycle.*
 import jp.co.cyberagent.dojo2020.DI
 import jp.co.cyberagent.dojo2020.data.model.FirebaseUserInfo
 import jp.co.cyberagent.dojo2020.data.model.Memo
-import jp.co.cyberagent.dojo2020.data.remote.auth.AuthDataSource
+import jp.co.cyberagent.dojo2020.data.remote.auth.FirebaseUserInfoDataSource
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 
 class HomeViewModel(context: Context) : ViewModel() {
     private val memoRepository = DI.injectRepository(context)
-    private val userInfoRepository: AuthDataSource = TODO()
+    private val firebaseUserInfoRepository: FirebaseUserInfoDataSource = TODO()
 
     val userInfoLiveData = liveData<FirebaseUserInfo> {
-        emitSource(userInfoRepository.fetchFirebaseUserInfo().asLiveData())
+        emitSource(firebaseUserInfoRepository.fetchFirebaseUserInfo().asLiveData())
     }
 
     val memoListLiveData = liveData<List<Memo>> {

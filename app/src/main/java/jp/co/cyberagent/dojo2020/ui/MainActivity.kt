5c26d14fa77df002a9c24e586e0e7c247b59e424
ShebangDog <e1n18088@oit.ac.jp>
2020-09-03 22:43:08 +0900

    MainActivityでRecyclerViewを設定する

--- app/src/main/java/jp/co/cyberagent/dojo2020/ui/MainActivity.kt
+++ app/src/main/java/jp/co/cyberagent/dojo2020/ui/MainActivity.kt
@@ -1,12 +1,25 @@
 package jp.co.cyberagent.dojo2020.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import jp.co.cyberagent.dojo2020.R
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import jp.co.cyberagent.dojo2020.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        with(binding) {
+            recyclerView.apply {
+                layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+                adapter = MemoAdapter((1 until 10).toList().map { it.toString() })
+            }
+        }
+
     }
 }

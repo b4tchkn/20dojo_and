1b70e342e8fcdf8bfff3d2401be811a2f03f4b40
ShebangDog <e1n18088@oit.ac.jp>
2020-09-03 15:01:42 +0900

    FirebaseAuthをサービスとして実装する

---
+++ app/src/main/java/jp/co/cyberagent/dojo2020/data/remote/FirebaseAuthentication.kt
@@ -0,0 +1,38 @@
+package jp.co.cyberagent.dojo2020.data.remote
+
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+object FirebaseAuthentication {
+
+    private val firebaseAuth = Firebase.auth
+
+    fun firebaseUser() = firebaseAuth.currentUser
+
+    fun signIn(
+        email: String,
+        password: String,
+        consumer: (FirebaseUser) -> Unit
+    ) {
+        firebaseAuth.signInWithEmailAndPassword(email, password)
+            .addOnSuccessListener { result ->
+                val user = result.user ?: return@addOnSuccessListener
+
+                consumer(user)
+            }
+    }
+
+    fun signUp(
+        email: String,
+        password: String,
+        consumer: (FirebaseUser) -> Unit
+    ) {
+        firebaseAuth.createUserWithEmailAndPassword(email, password)
+            .addOnSuccessListener { result ->
+                val user = result.user ?: return@addOnSuccessListener
+
+                consumer(user)
+            }
+    }
+}

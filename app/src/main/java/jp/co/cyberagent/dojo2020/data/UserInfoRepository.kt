cd4d7b6773467602166c9e5f8455045a3cdbd076
ShebangDog <e1n18088@oit.ac.jp>
2020-09-07 10:25:43 +0900

    ユーザー情報を持つUserInfoRepositoryを実装する

---
+++ app/src/main/java/jp/co/cyberagent/dojo2020/data/UserInfoRepository.kt
@@ -0,0 +1,39 @@
+package jp.co.cyberagent.dojo2020.data
+
+import android.content.ContentValues.TAG
+import android.util.Log
+import com.google.firebase.auth.FirebaseUser
+import jp.co.cyberagent.dojo2020.data.model.FirebaseUserInfo
+import jp.co.cyberagent.dojo2020.data.remote.auth.FirebaseAuthentication
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
+interface UserInfoRepository {
+    fun fetchUserInfo(): Flow<FirebaseUserInfo?>
+}
+
+class DefaultUserInfoRepository : UserInfoRepository {
+    init {
+        FirebaseAuthentication.addStateListener { firebaseAuth ->
+            firebaseUser = firebaseAuth.currentUser
+        }
+    }
+
+    private var firebaseUser: FirebaseUser? = null
+
+    override fun fetchUserInfo(): Flow<FirebaseUserInfo?> = flow {
+        if (firebaseUser == null || firebaseUser?.uid == null || firebaseUser?.email == null) {
+            emit(null)
+            return@flow
+        }
+
+        val user = firebaseUser ?: return@flow
+
+        val email = user.email ?: return@flow
+        val uid = user.uid
+
+        emit(FirebaseUserInfo(uid, email))
+        Log.d(TAG, "Emit $uid $email")
+    }
+
+}
--- app/src/main/java/jp/co/cyberagent/dojo2020/data/remote/auth/FirebaseUserInfoDataSource.kt
+++
@@ -1,8 +0,0 @@
-package jp.co.cyberagent.dojo2020.data.remote.auth
-
-import jp.co.cyberagent.dojo2020.data.model.FirebaseUserInfo
-import kotlinx.coroutines.flow.Flow
-
-interface FirebaseUserInfoDataSource {
-    fun fetchFirebaseUserInfo(): Flow<FirebaseUserInfo>
-}

977981e522c99d1fcd7ede6ea4935601bb3ba3f7
asagi71 <n.tomo9613@gmail.com>
2020-09-08 18:23:44 +0900

    TestProfileRepositoryのリファクタリング

--- app/src/main/java/jp/co/cyberagent/dojo2020/test/TestProfileRepository.kt
+++ app/src/main/java/jp/co/cyberagent/dojo2020/test/TestProfileRepository.kt
@@ -8,16 +8,17 @@ import jp.co.cyberagent.dojo2020.data.model.Profile
 import kotlinx.coroutines.flow.flow
 
 object TestProfileRepository : ProfileRepository {
-    override suspend fun saveProfile(uid: String, profile: Profile) {
+
+    override suspend fun saveProfile(uid: String?, profile: Profile) {
         Log.d(TAG, profile.toString())
     }
 
-    override suspend fun fetchProfile(uid: String) = flow {
+    override suspend fun fetchProfile(uid: String?) = flow {
         val twitter = Account("twitter", "twitterID", "twitter.com")
         val github = Account("github", "githubID", "github.com")
         val accountList = listOf(twitter, github)
 
-        val profile = Profile("USERNAME", "url", accountList)
+        val profile = Profile("USERNAME", "iconURL", accountList)
         emit(profile)
     }
 }

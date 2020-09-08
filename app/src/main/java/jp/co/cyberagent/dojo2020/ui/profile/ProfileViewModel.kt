e33f1dc7a273adf8f6afeb5ac986bdae14a99625
asagi71 <n.tomo9613@gmail.com>
2020-09-08 18:26:22 +0900

    ProfileViewModelでinjectするリポジトリをテスト用に変更

--- app/src/main/java/jp/co/cyberagent/dojo2020/ui/profile/ProfileViewModel.kt
+++ app/src/main/java/jp/co/cyberagent/dojo2020/ui/profile/ProfileViewModel.kt
@@ -6,26 +6,23 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import jp.co.cyberagent.dojo2020.DI
-import jp.co.cyberagent.dojo2020.data.DefaultUserInfoRepository
-import jp.co.cyberagent.dojo2020.data.UserInfoRepository
+import jp.co.cyberagent.dojo2020.data.ProfileRepository
+import jp.co.cyberagent.dojo2020.data.model.Profile
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 
 class ProfileViewModel(context: Context) : ViewModel() {
-    private val userInfoRepository = DefaultUserInfoRepository()
-    private val firebaseUserInfoRepository: UserInfoRepository =
-        DI.injectDefaultUserInfoRepository()
 
-    private fun user() = firebaseUserInfoRepository.fetchUserInfo()
+    private val firebaseUserInfoRepository: ProfileRepository =
+        DI.injectTestProfileRepository()
 
     private val mutableLiveData: MutableLiveData<Profile> = MutableLiveData()
     val liveData: LiveData<Profile>
         get() = mutableLiveData
 
     fun fetchUserData() = viewModelScope.launch {
-        userInfoRepository.fetchUserInfo().collect {
-            val userData = Profile("abcde", "12345")
-            mutableLiveData.value = userData
+        firebaseUserInfoRepository.fetchProfile(null).collect {
+            mutableLiveData.value = it
         }
     }
 }

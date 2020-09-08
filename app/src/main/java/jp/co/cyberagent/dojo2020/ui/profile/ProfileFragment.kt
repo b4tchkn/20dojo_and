be6bfa8dc232f6e8ac4fc3efa37ded9677220b75
asagi71 <n.tomo9613@gmail.com>
2020-09-08 22:06:57 +0900

    account関連データをnullableに変更

--- app/src/main/java/jp/co/cyberagent/dojo2020/ui/profile/ProfileFragment.kt
+++ app/src/main/java/jp/co/cyberagent/dojo2020/ui/profile/ProfileFragment.kt
@@ -5,12 +5,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import jp.co.cyberagent.dojo2020.R
 import jp.co.cyberagent.dojo2020.databinding.FragmentProfileBinding
 
 class ProfileFragment : Fragment() {
     private lateinit var binding: FragmentProfileBinding
+
+    private val viewModel: ProfileViewModel by viewModels {
+        ProfileViewModelFactory(this, Bundle(), requireContext())
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -28,6 +33,21 @@ class ProfileFragment : Fragment() {
         with(binding) {
             profileToHomeButton.setOnClickListener {
                 findNavController().navigate(R.id.action_profileFragment_to_homeFragment)
+            }
+
+            viewModel.liveData.observe(viewLifecycleOwner) {
+                profileUserName.text = it.name
+                profileIconUrl.text = it.iconUrl
+                val twitterAccount = it.accountList?.first { it.serviceName == "twitter" }
+                profileTwitterId.text = twitterAccount?.id
+                profileTwitterUrl.text = twitterAccount?.url
+                val githubAccount = it.accountList?.first { it.serviceName == "github" }
+                profileGithubId.text = githubAccount?.id
+                profileGithubUrl.text = githubAccount?.url
+            }
+
+            reloadButton.setOnClickListener {
+                viewModel.fetchUserData()
             }
         }
 

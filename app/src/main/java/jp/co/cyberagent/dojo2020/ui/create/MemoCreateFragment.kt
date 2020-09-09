0d6b72a2826bbe9d84ed014d919e7ecb32b589df
ShebangDog <e1n18088@oit.ac.jp>
2020-09-09 10:39:38 +0900

    spinnerの項目をユーザーが追加できる型にする

--- app/src/main/java/jp/co/cyberagent/dojo2020/ui/create/MemoCreateFragment.kt
+++ app/src/main/java/jp/co/cyberagent/dojo2020/ui/create/MemoCreateFragment.kt
@@ -8,7 +8,6 @@ import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
 import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
 import jp.co.cyberagent.dojo2020.R
 import jp.co.cyberagent.dojo2020.databinding.FragmentMemoCreateBinding
 
@@ -34,13 +33,10 @@ class MemoCreateFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         with(binding) {
-            val adapter = ArrayAdapter.createFromResource(
-                requireContext(),
-                R.array.spinner_item,
-                android.R.layout.simple_spinner_item
-            )
+            val adapter = SpinnerAdapter.getInstance(requireContext()).apply {
+                setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            }
 
-            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
             categorySpinner.adapter = adapter
 
             addButton.setOnClickListener {
@@ -55,5 +51,18 @@ class MemoCreateFragment : Fragment() {
         val manager = activity.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
 
         manager.showSoftInput(binding.titleTextEdit, InputMethodManager.SHOW_IMPLICIT)
+    }
+}
+
+object SpinnerAdapter {
+    private var INSTANCE: ArrayAdapter<String>? = null
+
+    fun getInstance(context: Context): ArrayAdapter<String> {
+        if (INSTANCE != null) return INSTANCE!!
+        val stringList = context.resources.getStringArray(R.array.spinner_item).toMutableList()
+
+        INSTANCE = ArrayAdapter(context, android.R.layout.simple_spinner_item, stringList)
+
+        return INSTANCE!!
     }
 }

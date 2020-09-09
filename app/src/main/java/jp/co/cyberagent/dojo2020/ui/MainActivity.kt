1c1d4fda83ec0eafe6617d06f8dac55152e017f2
ShebangDog <e1n18088@oit.ac.jp>
2020-09-09 23:24:40 +0900

    navigation controllerをactivityで持つようにする

--- app/src/main/java/jp/co/cyberagent/dojo2020/ui/MainActivity.kt
+++ app/src/main/java/jp/co/cyberagent/dojo2020/ui/MainActivity.kt
@@ -1,35 +1,23 @@
 package jp.co.cyberagent.dojo2020.ui
 
 import android.os.Bundle
-import android.view.Menu
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import jp.co.cyberagent.dojo2020.R
 import jp.co.cyberagent.dojo2020.databinding.ActivityMainBinding
-import jp.co.cyberagent.dojo2020.ui.create.MemoCreateFragment
-import jp.co.cyberagent.dojo2020.ui.home.HomeViewModel
-import jp.co.cyberagent.dojo2020.ui.home.HomeViewModelFactory
-import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.fragment_home.*
-import kotlinx.android.synthetic.main.fragment_memo_create.*
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private val homeViewModel by viewModels<HomeViewModel> {
-        HomeViewModelFactory(this, Bundle(), this)
-    }
+    private lateinit var navigationController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        navigationController = findNavController(R.id.navigation_host_fragment)
 
         setSupportActionBar(binding.toolbar)
-
-
     }
-
-
 
 }

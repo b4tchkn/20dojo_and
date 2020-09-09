4bc9666b2e08e05c81b237d2b2e7d6cdd9694d63
asagi71 <n.tomo9613@gmail.com>
2020-09-09 18:23:12 +0900

    円グラフの作成と一時的な時間取得用メソッドの追加

--- app/src/main/java/jp/co/cyberagent/dojo2020/ui/profile/ProfileViewModel.kt
+++ app/src/main/java/jp/co/cyberagent/dojo2020/ui/profile/ProfileViewModel.kt
@@ -6,23 +6,39 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import jp.co.cyberagent.dojo2020.DI
+import jp.co.cyberagent.dojo2020.data.DefaultUserInfoRepository
 import jp.co.cyberagent.dojo2020.data.ProfileRepository
+import jp.co.cyberagent.dojo2020.data.UserInfoRepository
 import jp.co.cyberagent.dojo2020.data.model.Profile
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 
 class ProfileViewModel(context: Context) : ViewModel() {
 
-    private val firebaseUserInfoRepository: ProfileRepository =
-        DI.injectTestProfileRepository()
+    private val firebaseUserInfoRepository: UserInfoRepository = DefaultUserInfoRepository()
+    private val firebaseProfileRepository: ProfileRepository = DI.injectTestProfileRepository()
+    private val memoRepository = DI.injectDefaultMemoRepository(context)
+
+    private fun user() = firebaseUserInfoRepository.fetchUserInfo()
 
     private val mutableLiveData: MutableLiveData<Profile> = MutableLiveData()
     val liveData: LiveData<Profile>
         get() = mutableLiveData
 
     fun fetchUserData() = viewModelScope.launch {
-        firebaseUserInfoRepository.fetchProfile(null).collect {
-            mutableLiveData.value = it
+        user().collect { userInfo ->
+            firebaseProfileRepository.fetchProfile(userInfo?.uid).collect {
+                mutableLiveData.value = it
+            }
+        }
+    }
+
+    fun fetchStudyTime() = viewModelScope.launch {
+        user().collect { userInfo ->
+            memoRepository.fetchAllMemo(userInfo?.uid).collect { memoList ->
+                //val noneList = memoList.filter { it.category == "none"}
+                //Log.d(TAG, noneList.first().toString())
+            }
         }
     }
 }

263c9bc4fa42a5bf5ec3bf5f58d9e63ab46ae84f
asagi71 <n.tomo9613@gmail.com>
2020-09-09 18:53:15 +0900

    インターフェースをMemoRepositoryに変更

--- app/src/main/java/jp/co/cyberagent/dojo2020/test/FakeRepository.kt
+++ app/src/main/java/jp/co/cyberagent/dojo2020/test/FakeRepository.kt
@@ -1,27 +1,27 @@
 package jp.co.cyberagent.dojo2020.test
 
 import android.util.Log
-import jp.co.cyberagent.dojo2020.data.local.MemoDataSource
+import jp.co.cyberagent.dojo2020.data.MemoRepository
 import jp.co.cyberagent.dojo2020.data.model.Memo
 import kotlinx.coroutines.flow.flow
 
-object FakeRepository : MemoDataSource {
-    override suspend fun saveMemo(memo: Memo) {
+object FakeRepository : MemoRepository {
+    override suspend fun saveMemo(uid: String?, memo: Memo) {
         Log.d(Constants.REPOSITORY, "insert: $memo")
     }
 
-    override suspend fun fetchAllMemo() = flow {
+    override suspend fun fetchAllMemo(uid: String?) = flow {
         val dataList = MemoData.list
 
         dataList.forEach { Log.d(Constants.REPOSITORY, "fetchAll: $it") }
         emit(dataList)
     }
 
-    override suspend fun fetchMemoById(id: String) = flow {
+    override suspend fun fetchMemoById(uid: String?, id: String) = flow {
         emit(MemoData.list.firstOrNull { it.id == id })
     }
 
-    override suspend fun deleteMemoById(id: String) {
+    override suspend fun deleteMemoById(uid: String?, id: String) {
         MemoData.list.removeIf { it.id == id }
     }
 }

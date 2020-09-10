4892ff8760fd5a017429bff1155fc8358ba399d2
aaa165 <b1802067@planet.kanazawa-it.ac.jp>
2020-09-10 13:27:33 +0900

    アクティビティ

--- app/src/main/java/jp/co/cyberagent/dojo2020/ui/MainActivity.kt
+++ app/src/main/java/jp/co/cyberagent/dojo2020/ui/MainActivity.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
 import jp.co.cyberagent.dojo2020.R
 import jp.co.cyberagent.dojo2020.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -16,8 +18,8 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         navigationController = findNavController(R.id.navigation_host_fragment)
-
         setSupportActionBar(binding.toolbar)
+
     }
 
 }
